91eeb4ec6b984d06d81ba62ae68608adf0e738ff
Musabbir Arrafi <67402551+musabbir-arrafi@users.noreply.github.com>
2026-04-11 17:49:49 -0400

    updated project card

--- js/projects.js
+++ js/projects.js
@@ -4,20 +4,22 @@ const projectsData = [
     slug: "pinn-autonomous-navigation",
     description: "A hybrid neural network model that integrates physics priors with learned dynamics components",
     image: "images/rudy-f1tenth-blue.png",
-    people: ["Musabbir A. Arrafi"],
-    tags: ["Autonomous Navigation", "PINNs", "Vehicle Dynamics"],
+    people: ["Musabbir A. Arrafi", "Malik Ali", "Dr, Krishna B. Kidambi (Advisor)", "Dr. Niki Stiffler (Advisor)"],
+    tags: ["Autonomous Navigation", "PINNs", "Vehicle Dynamics and Control"],
     paper: "#"
-    },  
+    },
+
     {
         title: "Control Evaluation with Learned Models",
         slug: "control-evaluation-learned-models",
         description: "Control algorithm performance evaluation via open-loop learned dynamic models",
         image: "images/projects/cape.jpg"
     },
-  {
-    title: "Energy Aware Control",
-    slug: "energy-aware-control",
-    description: "Predictive path planning for quadrotor UAVs",
-    image: "images/projects/energy-aware-control.jpg"
-  }
+    
+    {
+        title: "Energy Aware Control",
+        slug: "energy-aware-control",
+        description: "Predictive path planning for quadrotor UAVs",
+        image: "images/projects/energy-aware-control.jpg"
+    }
 ];
