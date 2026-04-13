1b3ecd78ae714fc1f614b43c50eff4267cf30239
Musabbir Arrafi <67402551+musabbir-arrafi@users.noreply.github.com>
2026-04-13 07:02:29 -0400

    smooth transition between slides

--- js/video-slider.js
+++ js/video-slider.js
@@ -3,7 +3,7 @@ let videoElement;
 const clips = Array.from({ length: 16 }, (_, i) => {
   const name = `clip_${String(i).padStart(2, "0")}`;
   return {
-    webm: `videos/clips/${name}.webm`,
+    // webm: `videos/clips/${name}.webm`,
     mp4: `videos/clips/${name}.mp4`
   };
 });
@@ -18,7 +18,7 @@ function loadVideo(index) {
 
   setTimeout(() => {
     videoElement.innerHTML = `
-      <source src="${clip.webm}" type="video/webm">
+      // <source src="${clip.webm}" type="video/webm">
       <source src="${clip.mp4}" type="video/mp4">
     `;
 
